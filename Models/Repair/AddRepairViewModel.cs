e9b40c030e90c22a28b349a27516c2904fd73645
R.Dimitrov <R.Dimitrov.1993@gmail.com>
2023-07-09 09:45:16 +0300

    add edit and finish repair to viewRepair view and fix view layout

--- Models/Repair/AddRepairViewModel.cs
+++ Models/Repair/AddRepairViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace CarStory.Models.Repair
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarStory.Models.Repair
 {
     public class AddRepairViewModel
     {
@@ -10,6 +12,8 @@
 
         public int CarMilleage { get; set; }
 
+        [Required]
+        [StringLength(2000, ErrorMessage = "Description must be between 5 and 2000 characters long", MinimumLength = 5)]
         public string Description { get; set; }
 
         public string CarRepairShopId { get; set; }
